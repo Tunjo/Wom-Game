bbbfc270dc0d615c3267bf57f938fc0be58d3684
Tunjo <antun.franjin@gmail.com>
2018-08-24 01:47:32 +0200

    Add some shop options.

--- game.py
+++ game.py
@@ -41,7 +41,7 @@ class Warrior(Player):
             "magic": []
         }
         self.potion = 1
-        self.gold = 50
+        self.gold = 5000
         self.bag = []
         self.quest = []
         self.diamonds = []
@@ -104,6 +104,12 @@ def json_monster():
         npc = json.load(files)
 
 
+def json_load_shop():
+    with open("shop\shop.json") as file:
+        global shopitem
+        shopitem = json.load(file)
+
+
 def clear_screen():
     if sys.platform == "win32":
         os.system("cls")
@@ -157,6 +163,7 @@ def game_option():
     clear_screen()
     json_load()
     json_monster()
+    json_load_shop()
     txt_flush(data["option"])
     option = input(" >> ")
     if option == "1":
@@ -166,10 +173,96 @@ def game_option():
     elif option == "5":
         save_game()
     elif option == "6":
+        shop()
+    elif option == "7":
         main_manu()
     else:
-        print("DONT HAVE THAT OPTION!!!")
         game_option()
+
+
+def txt_item_shop_book(itemname, cost):
+    print((' Name: {}: {}').format(itemname, cost))
+
+
+
+def shop():
+    clear_screen()
+    print_stats()
+    txt_flush(data["shop"])
+    value = input(" >> ")
+    if value == "1":
+        clear_screen()
+        print_stats()
+        txt_flush(data["shop0"])
+        ans = input(" >> ")
+        if ans == "1":
+            for item in stats:
+                item["gold"] -= shopitem["potion"]
+                item["potion"] += 1
+            shop()
+        elif ans == "2":
+            shop()
+    elif value == "2":
+        clear_screen()
+        print_stats()
+        print()
+        txt_item_shop_book(shopitem["books"]["book0"]["name"], shopitem["books"]["book0"]["cost"])
+        txt_item_shop_book(shopitem["books"]["book1"]["name"], shopitem["books"]["book1"]["cost"])
+        txt_item_shop_book(shopitem["books"]["book2"]["name"], shopitem["books"]["book2"]["cost"])
+        txt_item_shop_book(shopitem["books"]["book3"]["name"], shopitem["books"]["book3"]["cost"])
+        txt_item_shop_book(shopitem["books"]["book4"]["name"], shopitem["books"]["book4"]["cost"])
+        txt_item_shop_book(shopitem["books"]["book5"]["name"], shopitem["books"]["book5"]["cost"])
+        print()
+        txt_flush(data["shop1"])
+        itemname = input(" >> ")
+        for item in stats:
+            if itemname == shopitem["books"]["book0"]["name"] and item["gold"] >= shopitem["books"]["book0"]["cost"]:
+                item["bag"].append(shopitem["books"]["book0"]["name"])
+                item["gold"] -= shopitem["books"]["book0"]["cost"]
+                shop()
+            elif itemname == shopitem["books"]["book1"]["name"] and item["gold"] >= shopitem["books"]["book1"]["cost"]:
+                item["bag"].append(shopitem["books"]["book1"]["name"])
+                item["gold"] -= shopitem["books"]["book1"]["cost"]
+                shop()
+            elif itemname == shopitem["books"]["book2"]["name"] and item["gold"] >= shopitem["books"]["book2"]["cost"]:
+                item["bag"].append(shopitem["books"]["book2"]["name"])
+                item["gold"] -= shopitem["books"]["book2"]["cost"]
+                shop()
+            elif itemname == shopitem["books"]["book3"]["name"] and item["gold"] >= shopitem["books"]["book3"]["cost"]:
+                item["bag"].append(shopitem["books"]["book3"]["name"])
+                item["gold"] -= shopitem["books"]["book3"]["cost"]
+                shop()
+            elif itemname == shopitem["books"]["book4"]["name"] and item["gold"] >= shopitem["books"]["book4"]["cost"]:
+                item["bag"].append(shopitem["books"]["book4"]["name"])
+                item["gold"] -= shopitem["books"]["book4"]["cost"]
+                shop()
+            elif itemname == shopitem["books"]["book5"]["name"] and item["gold"] >= shopitem["books"]["book5"]["cost"]:
+                item["bag"].append(shopitem["books"]["book0"]["name"])
+                item["gold"] -= shopitem["books"]["book5"]["cost"]
+                shop()
+            else:
+                clear_screen()
+                txt_flush(data["shop3"])
+                time.sleep(2)
+                shop()
+    elif value == "3":
+        pass
+    elif value == "4":
+        pass
+    elif value == "5":
+        pass
+    elif value == "6":
+        pass
+    elif value == "7":
+        pass
+    elif value == "8":
+        game_option()
+    else:
+        clear_screen()
+        txt_flush(data["invalid"])
+        time.sleep(2)
+        shop()
+
 
 def inventory():
     clear_screen()
@@ -215,6 +308,7 @@ def inventory():
                 elif equip == npc["winterslake"]["witch"]["drops"][3]:
                     del_and_add_items_weapon(equip)
                     item["dmg"] += npc["winterslake"]["witch"]["drop"]["staff"]
+                    inventory()
             else:
                 clear_screen()
                 print("Wrong entry!!!")
@@ -365,7 +459,6 @@ def drink_potion():
         else:
             txt_flush_fast(data["potion"])
             time.sleep(1)
-
         if item["hp"] >= item["maxhp"]:
             item["hp"] = item["maxhp"]
 
